d5484011fd9af5f146c6e22d8d503776ebe9d94a
Natalia Szewczyk <natalia.szewczyk@makingwaves.com>
2018-11-07 14:30:04 +0100

    Added gulp config and project structure

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var del = require('del');
+var gulp = require('gulp');
+var cache = require('gulp-cache');
+var cssnano = require('gulp-cssnano');
+var htmlMin = require('gulp-htmlmin');
+var gulpIf = require('gulp-if');
+var imageMin = require('gulp-imagemin');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var runSequence = require('run-sequence');
+
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/scss/**/*.scss', ['sass']);
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('app/css/**/*.css')
+        .pipe(cssnano())
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('minify-js', function() {
+    return gulp.src('app/js/**/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('minify-html', function() {
+    return gulp.src('app/**/*.html')
+        .pipe(htmlMin({
+            collapseBooleanAttributes: true,
+            conservativeCollapse: true,
+            collapseWhitespace: true,
+            preserveLineBreaks: true,
+            removeComments: true,
+            removeEmptyAttributes: true,
+            removeRedundantAttributes: true,
+            useShortDoctype: true
+        }))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src('app/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts'))
+});
+
+gulp.task('images', function() {
+    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(cache(imageMin({
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('clean:dist', function() {
+    return del.sync('dist');
+});
+
+gulp.task('cache:clear', function(callback) {
+    return cache.clearAll('callback');
+});
+
+gulp.task('test', function() {
+
+});
+
+gulp.task('build', function(callback) {
+    runSequence('clean:dist',
+        ['sass', 'images', 'fonts', 'minify-html', 'minify-css', 'minify-js'],
+        callback
+    )
+});
+
+gulp.task('default', function(callback) {
+    runSequence(['sass', 'watch'],
+        callback
+    )
+});
