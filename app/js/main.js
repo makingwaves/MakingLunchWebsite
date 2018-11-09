7043c629c8f368eb4eaa89a67f1f85aa1dbf7c02
Natalia Szewczyk <natalia.szewczyk@makingwaves.com>
2018-11-09 13:57:52 +0100

    Added script for lazy load

---
+++ app/js/main.js
@@ -0,0 +1,45 @@
+(function() {
+
+    let lazyJSSelector = "[js-lazy-load]";
+    let lazyClass = "lazy-load--show";
+    let noJSClass = "no-js";
+    let lazyOffset = 200;
+
+    function isUsingJS() {
+        document.querySelector("html").classList.remove(noJSClass);
+    }
+
+    function initLazyLoad() {
+        let lazyLoadElements = document.querySelectorAll(lazyJSSelector);
+
+        if (!lazyLoadElements.length) {
+            return;
+        }
+
+        lazyLoadElements.forEach(function(element) {
+            if (isInViewport(element, lazyOffset)) {
+                element.classList.add(lazyClass);
+            }
+
+            window.addEventListener("scroll", function() {
+                if (isInViewport(element, lazyOffset)) {
+                    element.classList.add(lazyClass);
+                }
+            });
+
+        });
+
+        function isInViewport($element, offset) {
+            let elementTop = $element.offsetTop + offset;
+            let elementBottom = elementTop + $element.offsetHeight;
+            let viewportTop = window.scrollY;
+            let viewportBottom = viewportTop + window.outerHeight;
+            return elementBottom > viewportTop && elementTop < viewportBottom;
+        };
+
+    };
+
+    isUsingJS();
+    initLazyLoad();
+
+})();
