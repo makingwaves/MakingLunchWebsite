1f5ae0a22f1dad7072b7d9edc438e8075230a189
Paweł Bród <pawel.brod@gmail.com>
2019-06-17 15:14:35 +0200

    upgrade gulp configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,97 +1,56 @@
-var del = require('del');
-var gulp = require('gulp');
-var cache = require('gulp-cache');
-var cssnano = require('gulp-cssnano');
-var htmlMin = require('gulp-htmlmin');
-var gulpIf = require('gulp-if');
-var imageMin = require('gulp-imagemin');
-var sass = require('gulp-sass');
-var uglify = require('gulp-uglify');
-var runSequence = require('run-sequence');
-var babel = require('gulp-babel');
+const { src, dest, series} = require('gulp');
+const sass = require('gulp-sass');
+const cssnano = require('gulp-cssnano');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const htmlMin = require('gulp-htmlmin');
+const imageMin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+const del = require('del');
 
-gulp.task('sass', function() {
-    return gulp.src('app/scss/**/*.scss')
+function clean() {
+    return del('dist');
+}
+
+function images() {
+    return src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+        .pipe(cache(imageMin({interlaced: true})))
+        .pipe(dest('dist/images'))
+}
+
+function fonts() {
+    return src('app/fonts/**/*')
+        .pipe(dest('dist/fonts'))
+}
+
+function css() {
+    return src('app/scss/**/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('app/css'))
-});
-
-gulp.task('watch', function() {
-    gulp.watch('app/scss/**/*.scss', ['sass']);
-});
-
-gulp.task('minify-css', function() {
-    return gulp.src('app/css/**/*.css')
         .pipe(cssnano())
-        .pipe(gulp.dest('dist/css'));
-});
+        .pipe(dest('dist/css'))
+}
 
-gulp.task('minify-js', function() {
-    return gulp.src('temp/**/*.js')
+function js() {
+    return src(['app/js/main.js',])
+        .pipe(babel({presets: ['es2015']}))
         .pipe(uglify())
-        .pipe(gulp.dest('dist/js'));
-});
+        .pipe(dest('dist/js'));
+}
 
-gulp.task('minify-html', function() {
-    return gulp.src('app/**/*.html')
+function html() {
+    return src('app/**/*.html')
         .pipe(htmlMin({
             collapseBooleanAttributes: true,
             conservativeCollapse: true,
             collapseWhitespace: true,
-            preserveLineBreaks: true,
+            preserveLineBreaks: false,
             removeComments: true,
             removeEmptyAttributes: true,
             removeRedundantAttributes: true,
             useShortDoctype: true
         }))
-        .pipe(gulp.dest('dist'));
-});
+        .pipe(dest('dist'));
+}
 
-gulp.task('fonts', function() {
-    return gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'))
-});
+exports.build = series(clean, js, css, html, fonts, images);
 
-gulp.task('images', function() {
-    return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
-        .pipe(cache(imageMin({
-            interlaced: true
-        })))
-        .pipe(gulp.dest('dist/images'))
-});
-
-gulp.task('clean:dist', function() {
-    return del.sync('dist');
-});
-
-gulp.task('clean:temp', function() {
-    return del.sync('temp');
-});
-
-gulp.task('cache:clear', function(callback) {
-    return cache.clearAll('callback');
-});
-
-gulp.task('babel', function() {
-    return gulp.src([
-            'app/js/main.js',
-            'js/*.js'
-        ])
-        .pipe(babel({presets: ['es2015']}))
-        .pipe(gulp.dest('temp'))
-});
-
-gulp.task('build', function(callback) {
-    runSequence('clean:dist',
-        ['sass', 'babel', 'images', 'fonts'],
-        ['minify-css', 'minify-js', 'minify-html'],
-        'clean:temp',
-        callback
-    )
-});
-
-gulp.task('default', function(callback) {
-    runSequence(['sass', 'watch'],
-        callback
-    )
-});
